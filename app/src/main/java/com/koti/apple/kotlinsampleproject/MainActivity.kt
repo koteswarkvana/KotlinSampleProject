fbd2300d57b0f2e704c80d3bcf402447e3d9c8ed
Koteswarao <kotesh@kvana.com>
2018-07-16 10:54:45 +0530

    added button functionality.

--- app/src/main/java/com/koti/apple/kotlinsampleproject/MainActivity.kt
+++ app/src/main/java/com/koti/apple/kotlinsampleproject/MainActivity.kt
@@ -2,11 +2,31 @@ package com.koti.apple.kotlinsampleproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    val namesList = arrayListOf("Suresh", "Ramesh", "Hari","Koti")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name_tv.setText("Hi Kotlin User >>>> ")
+        change_text_bt.setOnClickListener {
+            println("text button clicked >>> ")
+            // randomly getting the name value
+            /*val random = Random()
+            val randomName = random.nextInt(namesList.count())
+            name_tv.text = namesList[randomName]*/
+
+            /// Adding names from edit text when button clicked .
+            val nameEt = etr_name_et.text.toString()
+            namesList.add(nameEt)
+            etr_name_et.text.clear()
+            name_tv.text = nameEt
+            println(namesList)
+            ///
+        }
     }
 }
---
+++ app/src/main/java/com/koti/apple/kotlinsampleproject/VariablesInformation.kt
@@ -0,0 +1,9 @@
+package com.koti.apple.kotlinsampleproject
+
+fun main(args: Array<String>) {
+    var rawString :String  = "I am Raw String!"
+    val escapedString : String  = "I am escaped String!\n"
+
+    println("Hello!"+escapedString)
+    println("Hey!!"+rawString)
+}
