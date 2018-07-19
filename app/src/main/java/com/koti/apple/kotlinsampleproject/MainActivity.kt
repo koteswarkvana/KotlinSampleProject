2e5af625be477c23b8b3b473bd29baa142c585bb
Koteswarao <kotesh@kvana.com>
2018-07-19 21:48:33 +0530

    Added function declaration with return statement and switch case (when introduced) special symbols implemented.

--- app/src/main/java/com/koti/apple/kotlinsampleproject/MainActivity.kt
+++ app/src/main/java/com/koti/apple/kotlinsampleproject/MainActivity.kt
@@ -14,6 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        strFunctions()
+        var sampleStr = learnCouse("Swift and Kotlin")
+        var lengthOfArea = calculateArea(18, 16)
+        println(sampleStr +" is > "+lengthOfArea)
+        printCourseName("andr1oid")
+
+
         name_tv.setText("Hi Kotlin User >>>> ")
         change_text_bt.setOnClickListener {
             println("text button clicked >>> ")
@@ -51,6 +58,53 @@ class MainActivity : AppCompatActivity() {
             println("cVal value >> "+cVal)
 
             ///
+        }
+    }
+
+    fun strFunctions() {
+        // special characters included.
+        println("Learning Kotlin \n along with Swift")
+        println("Learning Kotlin \b along with Swift")
+        println("Learning Kotlin \t along with Swift")
+        println("Learning Kotlin \r along with Swift")
+        println("Learning Kotlin \$ along with Swift")
+        println("Learning Kotlin \\ along with Swift")
+
+        var courseName = """android development using Kotlin
+            |language and
+            |xml
+        """.trimMargin()
+
+        var compareStr = "hi".contentEquals("hi")
+        println("ccompare string is > "+compareStr)
+    }
+
+    // Here fun means function
+    // : String ===> means return type string
+    // courseName: String ===> parameter
+    // pass parameters in this function
+    fun learnCouse(courseName: String): String {
+        print(courseName)
+        return " Length of area " // print the output in single line because we are using the ===>  "print" statment.
+    }
+
+    // pass parameters in this function and returning the result
+    fun calculateArea(length: Int, breadth: Int): Int {
+        return length * breadth
+    }
+
+    // switch functionality implementing in this function
+    fun printCourseName(courseName: String) {
+        when (courseName) {
+            "java" -> { // Remember If you want to write multiple line of code need to use the curly " {} " braces .
+                print("Java language selected > \n")
+                println("Java contains new versions >> ")
+                var name = "Ramesh"
+                println("Employee name is > " + name)
+            }
+            "android" -> print("Android language selected > \n")
+            "swift" -> print("Swift language selected > \n")
+            else -> println("no language selected > ")
         }
     }
 }
